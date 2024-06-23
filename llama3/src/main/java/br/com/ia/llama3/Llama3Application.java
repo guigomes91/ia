486eab8411379f6b1444f2c140f9df8a678e20af
guigomes91 <guilherme.gomes91@outlook.com>
2024-06-23 16:08:56 -0300

    first commit

---
+++ llama3/src/main/java/br/com/ia/llama3/Llama3Application.java
@@ -0,0 +1,13 @@
+package br.com.ia.llama3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Llama3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Llama3Application.class, args);
+	}
+
+}
